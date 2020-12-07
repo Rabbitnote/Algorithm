81d4630d23e4b284b166b7d6f290ba582e50f834
note <note16651@hotmail.co.th>
2020-12-08 00:26:06 +0700

    [Update]Change Directory

--- Algorithm/Warshall.java
+++ Algorithm/Warshall.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WANDF {
+public class Warshall {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
